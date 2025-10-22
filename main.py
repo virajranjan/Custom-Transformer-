e416f75cce361f8b9b785f4337793f4a2a0f0bb5
virajranjan <imvikrantranjan@gmail.com>
2025-10-22 18:07:14 +0530

    Implemented the Encoderblock and Encoder

--- main.py
+++ main.py
@@ -108,3 +108,29 @@ class ResidualConnection(nn.Model):
         self. norm = Layer_Normalization()
     def forward(self, x, sublayer):
         return x+self.dropout(sublayer(self.norm(x)))
+
+
+# Encode block 
+class Encoderblock(nn.Module):
+    def __init__(self, self_attention_block:MultiheadAttentionBlock, feed_forward_block:Feed_forward, dropout:float)-> None:
+        super().__init__()
+        self .self_attention_block = self_attention_block
+        self. feed_forward_block = feed_forward_block
+        self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+    
+    def forward(self, x, src_mask):
+        x = self.residual_connection[0](x,lambda x:self.self_attention_block(x,x,x,src_mask))
+        x = self.residual_connection[1](x,self.feed_forward_block)
+        return x
+    
+# single tansformer can have muiltiple encoder block, we will define N Encoder.
+class Encoder(nn.Module):
+     def __init__(self, layers:nn.Module)->None:
+         super().__init__()
+         self.layer = layers
+         self.norm = Layer_Normalization()
+    
+     def forward(self,x,mask):
+         for layer  in self.layers:
+              x= layer(x,mask)
+         return self.norm(x)
