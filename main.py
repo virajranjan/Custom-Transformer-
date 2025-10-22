2904d77b1a407e56a6a315bf2598e5e0662b969f
virajranjan <imvikrantranjan@gmail.com>
2025-10-22 17:47:24 +0530

    Implemented residual connection + normalization

--- main.py
+++ main.py
@@ -56,5 +56,55 @@ class Feed_forward(nn.Module):
     
 # Multi-head attention  
 class MultiheadAttentionBlock(nn.Module):
-    def __init__(self,):
+
+    def __init__(self,d_model:int, dropout:float, h:int):
         super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert  d_model%h ==0, "d_model is not divisible by h"
+
+        self.d_k = d_model//h
+        self.w_q = nn.Linear(d_model, d_model) #Wq
+        self.w_k = nn.Linear(d_model, d_model) #Wk
+        self.w_v = nn.Linear(d_model, d_model) #Wv
+
+        self.w_o = nn.Linear(d_model, d_model) #W0
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value,mask, dropout:nn.Dropout):
+        d_k = query.shape[-1]
+        #(batch,h,seq_len, seq_len)-->(batch, h, seq_len, seq_len)
+        attention_score = (query@ key.tanspose(-2,-1))/math.sqrt(d_k)
+        if mask is not None:
+            attention_score.masked_fill_(mask == 0,-1e9)
+        attention_score = attention_score.softmax(dim = -1) # (batch, h, seq_len , seq_len)\
+        if dropout is not None:
+            attention_score= dropout(attention_score)
+        return attention_score@value , attention_score
+
+    def forward(self, q , k , v ,mask):
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        query  = query.view(query.shape[0],query.shape[1],self.h, self.d_k).transpose(1,2)
+        key = key.view(key.shape[0],key.shape[1],self.h, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0],value.shape[1],self.h,self.d_k).transpose(1,2)
+
+        x, self.attentiom_score = MultiheadAttentionBlock.attention(query,key, value, mask , self.dropout)
+
+        # (batch,j,seq_len, d_k)--> (batch, seq_len, h, d_k) --> (batch, h, seq_len , d_k)
+        x = x.transpose(1,2).contigous().view(x.shape[0],-1,self.h*self.d_k)
+        #(batch, seq_len , d_model) --> (batch, seq_len, d_model)
+
+        return self.w_o(x)
+
+class ResidualConnection(nn.Model):
+
+    def __init__(self, dropout:float)->None:
+        super().__init()
+        self.dropout = nn.Dropout(dropout)
+        self. norm = Layer_Normalization()
+    def forward(self, x, sublayer):
+        return x+self.dropout(sublayer(self.norm(x)))
