a3c3c525658b2ac9e3db2c019537acbbd2e78530
virajranjan <imvikrantranjan@gmail.com>
2025-10-23 16:16:43 +0530

    implemented decoder block and decoder

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ class MultiheadAttentionBlock(nn.Module):
 
         return self.w_o(x)
 
-class ResidualConnection(nn.Model):
+class ResidualConnection(nn.Module):
 
     def __init__(self, dropout:float)->None:
         super().__init()
@@ -134,3 +134,26 @@ class Encoder(nn.Module):
          for layer  in self.layers:
               x= layer(x,mask)
          return self.norm(x)
+
+# implementing the decoder block 
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block :MultiheadAttentionBlock, cross_attention_block:MultiheadAttentionBlock,feed_forward_block:Feed_forward, dropout:float)->None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = self.cross_attention_block
+        self.feed_forward_block = self.feed_forward_block
+        self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+    
+    def forward(self,x, encoder_output,src_mask,tgt_mask):
+        x = self.residual_connection[0](x,lambda x:self.self_attention_block(x,x,x,tgt_mask))
+        x = self.residual_connection[1](x, lambda x:self.cross_attention_block(x,encoder_output,encoder_output,src_mask))
+        x = self.residual_connection[2](x,self.feed_forward_block)
+
+class Decoder(nn.Module):
+    def __init__(self, layers:nn.Module)->None:
+        super().__init__()
+        self.layers = layers
+        self.norm = Layer_Normalization
+
+        def forward(self, x ,mask):
+            pass
